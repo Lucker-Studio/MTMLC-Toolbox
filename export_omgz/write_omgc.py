3a710e1c455fa8bd3420ee6654bc7d8d6e47bcbc
ganpig <ganpig@qq.com>
2022-10-26 16:35:46 +0800

    upd

--- export_omgz/write_omgc.py
+++ export_omgz/write_omgc.py
@@ -8,7 +8,7 @@ def write_omgc(instructions: list, omgc_path: str) -> None:
     将指令列表写入 omgc 谱面文件。
     """
     if DEBUG_MODE:
-        debug_log = open('debug.log', 'w')
+        debug_log = open('debug.log', 'w', encoding='utf-8')
     instructions_expanded = [len(instructions)]  # 指令个数
     for time, instr_type, parameters in instructions:  # 此处指令已排序
         instructions_expanded.extend((time, instr_type, len(parameters), *parameters))  # 将二维列表展开成一维并添加参数数量
