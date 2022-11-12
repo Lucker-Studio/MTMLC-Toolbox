3e3f304fe10b83cb845a59f00f19e74438dae420
ganpig <ganpig@qq.com>
2022-11-12 11:23:13 +0800

    upd

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-pyinstaller Omegar-Toolbox.spec --distpath build --workpath build\temp
-cd build
-rd /s /q temp
-ren Omegar-Toolbox.exe Omegar-Toolbox-Windows.exe
--- build.sh
+++
@@ -1,4 +0,0 @@
-pyinstaller Omegar-Toolbox.spec --distpath build --workpath build/temp
-cd build
-rm -r temp
-mv Omegar-Toolbox Omegar-Toolbox-Linux
