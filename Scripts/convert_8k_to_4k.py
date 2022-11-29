b39aa023f04d94c49f53f338f16bc3e1b666472e
ganpig <ganpig@qq.com>
2022-11-29 23:35:16 +0800

    upd

--- Scripts/convert_8k_to_4k.py
+++ Scripts/convert_8k_to_4k.py
@@ -1,6 +1,9 @@
 import os
-from Core.file_io import read_json, save_file, write_json
+from copy import deepcopy
+
 import easygui
+
+from Core.file_io import read_json, save_file, write_json
 
 # 目标轨道数
 TARGET_NUMBER_OF_TRACKS = 4
@@ -14,8 +17,8 @@ def main() -> None:
         project_data = read_json(file_path)
         new_line_list = []
         for line in project_data['line_list']:
-            new_line_up = dict(line)  # 拷贝
-            new_line_down = dict(line)
+            new_line_up = deepcopy(line)
+            new_line_down = deepcopy(line)
             new_line_up['note_list'] = []
             new_line_down['note_list'] = []
             for change in new_line_down['speed_changes']:
@@ -23,8 +26,9 @@ def main() -> None:
             for note in line['note_list']:
                 if note['judging_track'] >= TARGET_NUMBER_OF_TRACKS:
                     note['judging_track'] %= TARGET_NUMBER_OF_TRACKS
-                    for change in note['showing_track_changes']:
-                        change[1] %= TARGET_NUMBER_OF_TRACKS
+                    if 'showing_track_changes' in note:
+                        for change in note['showing_track_changes']:
+                            change['target'] %= TARGET_NUMBER_OF_TRACKS
                     new_line_down['note_list'].append(note)
                 else:
                     new_line_up['note_list'].append(note)
@@ -33,3 +37,4 @@ def main() -> None:
         project_data['line_list'] = new_line_list
         if new_file_path := save_file('保存工程文件', '.mtmlproj', os.path.splitext(file_path)[0]+'_4K'):
             write_json(project_data, new_file_path)
+            easygui.msgbox('转换成功！', '8K 转双面 4K', '好的')
--- Scripts/open_file/mtmlproj.py
+++ Scripts/open_file/mtmlproj.py
@@ -1,19 +1,34 @@
 import os
+import tempfile
 
 import easygui
 
 from Core.chart_compiler import compile_chart
-from Core.file_io import read_json, write_mtmlc
+from Core.chart_previewer import preview_chart
+from Core.file_io import pack_folder, read_json, write_mtmlc
+from Core.project_exporter import export_project
 
 
 def handle_mtmlproj(file_path: str) -> None:
     """
     处理 mtmlproj 文件
     """
-    if choice := easygui.buttonbox('请选择要对此文件执行的操作', file_path, ('编译为 mtmlc',)):
+    op = ['编译为 mtmlc']
+    file_dir = os.path.split(file_path)[0]
+    if 'index.mtmlinfo' in os.listdir(file_dir):
+        op.append('预览谱面')
+    if choice := easygui.buttonbox('请选择要对此文件执行的操作', file_path, op):
         if choice == '编译为 mtmlc':
             project_data = read_json(file_path)
             mtmlc_path = os.path.splitext(file_path)[0]+'.mtmlc'
             lines, notes, commands = compile_chart(project_data)
             write_mtmlc(lines, notes, commands, mtmlc_path)
             easygui.msgbox('编译成功！', '编译为 mtmlc', '好的')
+
+        elif choice == '预览谱面':
+            song_info = read_json(os.path.join(file_dir, 'index.mtmlinfo'))
+            song_info['charts'] = [{'difficulty': 'Default', 'writer': 'Unknown', 'path': file_path}]
+            files = export_project(**song_info, folder_path=os.path.split(file_path)[0])
+            chart_dir = tempfile.mkdtemp()
+            pack_folder(files, chart_dir)
+            preview_chart(chart_dir)
